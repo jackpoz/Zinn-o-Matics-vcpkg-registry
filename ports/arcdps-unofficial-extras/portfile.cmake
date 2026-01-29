9e2be400913e2555a1ae2e6ff86da434df3f26dc
Giacomo Pozzoni <giacomopoz@gmail.com>
2026-01-29 23:46:09 +0100

    Update arcdps-unofficial-extras port

--- ports/arcdps-unofficial-extras/portfile.cmake
+++ ports/arcdps-unofficial-extras/portfile.cmake
@@ -3,9 +3,9 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO jackpoz/arcdps_unofficial_extras_releases
-    REF 2d95a06557e43d2b7c5ff29ed3eb34027af3fea5
-    SHA512 cbfbf7ee9fe377c0f01a4a5922aab34490088ece52656cd6eeee04041dac9235da452745162ac461d2bb2edc49cb27c2934e7c5101d46fc49f62ae3ac5cb518f
-    HEAD_REF master
+    REF fa7917e9b1ae2370376f5e4f0a352788b1469907
+    SHA512 278e142c73b5563b7fc526a489cc47b73748d22ab10a60e6631884319c156bcba1319bc908d4618ba29feef36a8c54d6a60e94b00cf262867d6fe57552bfa391
+    HEAD_REF crossplatform
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
